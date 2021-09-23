3ac3fc7586c55f35d2517dc04b6d744da626bf0f
tumar77 <tumarnyshanova1981@gmail.com>
2021-09-23 14:09:59 +0600

    update

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ $(".slider").slick({
     slidesToShow: 3,
     dots:true,
     autoplay:true,
-    autoplaySpeed:1000
+    autoplaySpeed:1000,
+
+    responsive:[
+        {
+        breakpoint: 768,
+        settings: {
+            arrows:false,
+            slidesToShow:1,
+            autoplay:false
+        }
+    }]
 })
